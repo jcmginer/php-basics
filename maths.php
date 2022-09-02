3e73f37eaf27c1b50e6812303869f9a7339ef3f4
Juan Carlos M Giner <jcmginer@gmail.com>
2022-09-02 16:25:03 +0200

    Added maths.php

---
+++ maths.php
@@ -0,0 +1,18 @@
+<?php
+
+$abs = -3;
+echo(abs($abs) . "<br>");
+
+$round = 1.70;
+echo(round($round) . "<br>");
+
+$max = [2, 3, 6, 19];
+echo(max($max) . "<br>");
+
+$min = [2, 3, 6, 19];
+echo(min($min) . "<br>");
+
+$random = rand(10,100);
+echo $random . "<br>";
+
+?>
